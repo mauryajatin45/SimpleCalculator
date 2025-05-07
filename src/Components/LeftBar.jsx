ca3249af0312b466101f5a39d39feeaf2fdf1c14
Jatin Maurya <133239865+mauryajatin45@users.noreply.github.com>
2025-05-07 18:21:22 +0530

    Update LeftBar.jsx

--- src/Components/LeftBar.jsx
+++ src/Components/LeftBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function LeftBar() {
   return (
     <div className='w-1/4 bg-gray-500 h-screen flex justify-center items-center'>
-      <img src="./calculator.png" alt="Simple Calculator Image" className='w-4/6' />
+      <img src="www.mauryajatin.me/project/react/Calculator/calculator.png" alt="Simple Calculator Image" className='w-4/6' />
     </div>
   )
 }
